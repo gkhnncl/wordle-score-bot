b32026717c200a13eff680e57964fc6e545c61a5
AJ Carl Dy <ajcarl.dy@gmail.com>
2022-02-09 11:17:21 +0800

    Add logs file

--- bot.py
+++ bot.py
@@ -12,7 +12,9 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # Enable logging
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+    filename="logs.log",
 )
 
 logger = logging.getLogger(__name__)
