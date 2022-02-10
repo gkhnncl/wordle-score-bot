38c63e988176a9b0e07e7be4715ccd1134616549
AJ Carl Dy <ajcarl.dy@gmail.com>
2022-02-10 14:11:10 +0800

    Remove logs file

--- bot.py
+++ bot.py
@@ -12,9 +12,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # Enable logging
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-    filename="logs.log",
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
 logger = logging.getLogger(__name__)
