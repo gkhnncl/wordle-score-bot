33d63c1334e2d7393194afca4c725bb23989ec9c
AJ Carl Dy <ajcarl.dy@gmail.com>
2022-02-17 17:08:42 +0800

    Remove start and help commands

--- bot.py
+++ bot.py
@@ -25,16 +25,6 @@ load_dotenv()
 TOKEN = environ.get("TOKEN")
 
 
-def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text("Wordle score bot started!")
-
-
-def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text("Help!")
-
-
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
@@ -151,21 +141,13 @@ def main():
     """Start the bot."""
     updater = Updater(TOKEN, use_context=True)
 
-    # Get the dispatcher to register handlers
     dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("recap", recap))
     dp.add_handler(CommandHandler("weekly", weekly))
-
     dp.add_handler(MessageHandler(Filters.text, score_listener))
 
-    # log all errors
     dp.add_error_handler(error)
 
-    # Start the Bot
     updater.start_polling()
 
     updater.idle()
